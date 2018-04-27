1a4b56a7c49a4509857667a6dfa4e2cd60c6b82f
wangshuang <wangshuang@wangshuangdemac-mini.local>
2018-04-27 11:45:18 +0800

    Initial Commit

---
+++ JJTestGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JJTestGit
+//
+//  Created by wangshuang on 2018/4/27.
+//  Copyright © 2018年 wangshuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JJTestGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JJTestGit
+//
+//  Created by wangshuang on 2018/4/27.
+//  Copyright © 2018年 wangshuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
